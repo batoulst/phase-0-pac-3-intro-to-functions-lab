872259542e3fa40cf88c0d3494d752220dca4ccb
batoul sudki tenbakjy <batoul.tenbakji@gmail.com>
2022-10-15 12:52:45 +0300

    Edit completed assignment

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ function logWhisper(string){
     console.log( string.toLowerCase());
 }
 function sayHiToHeadphonedRoommate(string){
-if (string=='hello') {return "I can't hear you!"}
-if (string=='HELLO') {return "YES INDEED!"}
+if (string===string.toLowerCase()) {return "I can't hear you!"}
+if (string===string.toUpperCase()) {return "YES INDEED!"}
 if (string=="Let's have dinner together!") {return "I would love to!"}
 
 
 }
-
-//console.log(shout("hello"))
